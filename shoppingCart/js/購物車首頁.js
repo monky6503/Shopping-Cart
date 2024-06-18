9b3c573bc83cdc58f24eb280ff445a2d0b6066c6
Chun-Ming, Liao <97944035+monky6503@users.noreply.github.com>
2024-06-18 12:53:01 +0800

    update

--- shoppingCart/js/購物車首頁.js
+++ shoppingCart/js/購物車首頁.js
@@ -28,7 +28,6 @@
 // })
 
 const colorElement = document.querySelectorAll(".color");
-
 colorElement.forEach(button => button.addEventListener("click",function(){
   let imgElement = document.querySelector(".airpod-max");
   imgElement.src = `./shoppingCart/image/product-list/airpods-max-${this.name}.png`
