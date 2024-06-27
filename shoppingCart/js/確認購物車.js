42e4448a36663b08b9129609663a48ab62db7985
Chun-Ming, Liao <97944035+monky6503@users.noreply.github.com>
2024-06-27 14:42:17 +0800

    update

--- shoppingCart/js/確認購物車.js
+++ shoppingCart/js/確認購物車.js
@@ -1,22 +1,42 @@
 
+
+//確保回到這頁 先前選擇的數量還會在
+const items = JSON.parse(localStorage.getItem("Items"));
+if(items){
+  document.querySelectorAll('.js-input').forEach(input => {
+    let name = input.nextElementSibling.nextElementSibling.getAttribute("name");
+    input.value = items[name];
+});
+}
+
+updateCartQuantity();
+
 function updateCartQuantity(){
   const inputObject = document.querySelectorAll(".js-input");
   let cartQuantity = 0;
   let totalAmount = 0;
+  let items = {mbp:0,ipp:0,ip_15p:0};
 
   inputObject.forEach(input =>{
     const quantity = parseInt(input.value);
-    cartQuantity+=quantity;
     const priceElement = input.nextElementSibling.nextElementSibling;
+    let name = priceElement.getAttribute("name")
     const price = parseInt(priceElement.getAttribute('data-price'));
+
+    items[name] +=quantity;
+    cartQuantity+=quantity;
+
     priceElement.innerHTML = `NT$${(quantity * price).toLocaleString()}`;
     totalAmount += price * quantity;
-    
   })
 
   document.querySelector(".js-count").innerHTML = `${cartQuantity}`;
   document.querySelector(".js-amount").innerHTML = `NT$${(totalAmount).toLocaleString()}`;
   document.querySelector(".js-total").innerHTML = `NT$${(totalAmount).toLocaleString()}`;
+
+  localStorage.setItem("Items",JSON.stringify(items));
+  localStorage.setItem("Count",cartQuantity);
+  localStorage.setItem("Total",totalAmount);
 }
 
 
@@ -25,8 +45,10 @@ function updateCartQuantity(){
 document.querySelectorAll('.js-plus').forEach(button => {
   button.addEventListener('click', function() {
       let input = button.previousElementSibling;
-      input.value = parseInt(input.value) + 1;
-      updateCartQuantity()
+      let name = button.nextElementSibling;
+      let value = JSON.parse(localStorage.getItem("Items"))[name.getAttribute("name")];
+      input.value = parseInt(value) + 1;
+      updateCartQuantity();
   });
 });
 
@@ -34,8 +56,10 @@ document.querySelectorAll('.js-plus').forEach(button => {
 document.querySelectorAll('.js-minus').forEach(button => {
   button.addEventListener('click', function() {
       let input = button.nextElementSibling;
-      if (parseInt(input.value) > 0) {
-          input.value = parseInt(input.value) - 1;
+      let name = button.nextElementSibling.nextElementSibling.nextElementSibling;
+      let value = JSON.parse(localStorage.getItem("Items"))[name.getAttribute("name")]
+      if (value > 0) {
+          input.value = parseInt(value) - 1;
           updateCartQuantity();
       }
   });
@@ -49,13 +73,4 @@ document.querySelectorAll('.js-input').forEach(input => {
       updateCartQuantity();
   });
 });
-
-
-
-updateCartQuantity();
-
-
-
-
-
 
