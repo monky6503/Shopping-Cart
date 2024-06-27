4012bd6fe8e3fcb90ae4bb0e80deb09e236a6967
Chun-Ming, Liao <97944035+monky6503@users.noreply.github.com>
2024-06-27 11:33:31 +0800

    update

--- shoppingCart/js/購物車首頁.js
+++ shoppingCart/js/購物車首頁.js
@@ -1,32 +1,3 @@
-// const grayElement = document.querySelector(".space-gray");
-// const pinkElement = document.querySelector(".pink");
-// const greenElement = document.querySelector(".green");
-// const silverElement = document.querySelector(".silver");
-// const skyblueElement = document.querySelector(".sky-blue");
-// let imgElement = document.querySelector(".airpod-max");
-
-
-
-// grayElement.addEventListener("click",function(){
-//   imgElement.src = "./image/product-list/airpods-max-spacegray.png"
-// })
-
-// pinkElement.addEventListener("click",function(){
-//   imgElement.src = "./image/product-list/airpods-max-pink.png"
-// })
-
-// greenElement.addEventListener("click",function(){
-//   imgElement.src = "./image/product-list/airpods-max-green.png"
-// })
-
-// silverElement.addEventListener("click",function(){
-//   imgElement.src = "./image/product-list/airpods-max-silver.png"
-// })
-
-// skyblueElement.addEventListener("click",function(){
-//   imgElement.src = "./image/product-list/airpods-max-skyblue.png"
-// })
-
 const colorElement = document.querySelectorAll(".color");
 colorElement.forEach(button => button.addEventListener("click",function(){
   let imgElement = document.querySelector(".airpod-max");
